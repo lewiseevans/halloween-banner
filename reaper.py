5b2acc3962d532e0c6a78645241af767a7a979fb
Lewis Evans <lewis.evans@uk.ibm.com>
2021-10-05 09:16:08 +0100

    Is your time up?

--- reaper.py
+++ reaper.py
@@ -29,19 +29,85 @@ def draw():
     util.drawPixel(0, row, lightGrey)
     util.drawManyPixels(10, row, lightGrey, 5)
 
-    row += 1
+    row = 1
     util.drawPixel(0, row, lightGrey)
     util.drawPixel(7, row, brown)
     util.drawManyPixels(10, row, lightGrey, 4)
 
-    row += 1
+    row = 2
 
     util.drawPixel(0, row, lightGrey)
     util.drawPixel(8, row, brown)
     util.drawManyPixels(10, row, lightGrey, 3)
     util.drawPixel(14, row, darkGrey)
 
+    row = 3
+    util.drawManyPixels(0, row, lightGrey, 3)
+    util.drawPixel(9, row, brown)
+    util.drawPixel(11, row, lightGrey)
+    util.drawManyPixels(13, row, darkGrey, 2)
+
+    row = 4
+    util.drawManyPixels(0, row, lightGrey, 4)
+    util.drawPixel(10, row, brown)
+    util.drawManyPixels(12, row, darkGrey, 3)
+
+    row = 5
+    util.drawManyPixels(0, row, lightGrey, 5)
+    util.drawPixel(11, row, brown)
+    util.drawManyPixels(12, row, darkGrey, 2)
+    util.drawPixel(15, row, lightGrey)
+
+    row = 6
+    util.drawManyPixels(0, row, lightGrey, 3)
+    util.drawManyPixels(6, row, white, 3)
+    util.drawPixel(10, row, lightGrey)
+    util.drawPixel(12, row, brown)
+    util.drawManyPixels(14, row, lightGrey, 2)
+
+    row = 7
+    util.drawManyPixels(0, row, lightGrey, 3)
+    util.drawManyPixels(6, row, white, 3)
+    util.drawPixel(11, row, lightGrey)
+    util.drawManyPixels(14, row, lightGrey, 2)
+
+    row = 8
+    util.drawManyPixels(0, row, lightGrey,2)
+    util.drawManyPixels(4, row, white, 7)
+    util.drawManyPixels(12, row, lightGrey, 4)
+
+    row = 9
+    util.drawPixel(0, row, lightGrey)
+    util.drawPixel(4, row, white)
+    util.drawPixel(7, row, white)
+    util.drawPixel(10, row, white)
+    util.drawManyPixels(12, row, lightGrey, 4)
+
+    row = 10
+    util.drawPixel(4, row, white)
+    util.drawPixel(7, row, white)
+    util.drawPixel(10, row, white)
+    util.drawManyPixels(12, row, lightGrey, 4)
+
+    row = 11
+    util.drawManyPixels(4, row, white, 7)
+    util.drawManyPixels(12, row, lightGrey, 4)
+
+    row = 12
+    util.drawManyPixels(5, row, white, 5)
+    util.drawManyPixels(11, row, lightGrey, 5)
+
+    row = 13
+    util.drawManyPixels(0, row, lightGrey, 3)
+    util.drawManyPixels(7, row, white, 2)
+    util.drawManyPixels(10, row, lightGrey, 6)
+
+    row = 14
+    util.drawManyPixels(0, row, lightGrey, 4)
+    util.drawManyPixels(9, row, lightGrey, 7)
+
+    row = 15
+    util.drawManyPixels(1, row, lightGrey, 16)
 
     unicornhathd.show()
-
     time.sleep(5)
